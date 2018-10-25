fdeabe6069450c5074ce86b813eb79da33f80a04
Konstantin Dolbilov <dolbilov@galaxy-labs.ru>
2018-10-25 12:49:27 +0300

    highlight persists while clicking background

--- src/index.js
+++ src/index.js
@@ -4,20 +4,14 @@ import Annotation from '@components/Annotation'
 const canvas = document.getElementById('tester');
 const controller = new SceneManager(canvas);
 
-if( !window.localStorage.annotations ) {
-	window.localStorage.setItem('annotations', JSON.stringify({}));
-}
-
 const state = {
 	action: null,
-	// annotations: JSON.parse(window.localStorage.annotations), //not yet
 	annotations: {},
 	MODES: {
 		moving: 1,
 		placed: 2
 	},
 };
-
 
 controller.restoreVisibility = () => {
 	controller.scene.traverse((object) => {
@@ -63,17 +57,9 @@ controller.load('./src/models/gltf/heart_animated2.gltf', () => {
 				selected = intersected;
 				meshName.innerHTML = intersection.object.name.replace(/_/g, " ");
 			}
-		} else {
-			if (intersected) intersected.material.emissive.setHex(intersected.currentHex);
-			intersected = null;
 		}
 
 	}, false);
-	
-	for (let key in state.annotations) {
-		const item = state.annotations[key];
-		console.log(item);
-	}
 });
 
 function addAnnotation(e) {
@@ -114,10 +100,6 @@ function addAnnotation(e) {
 					if (e.target.matches('.delete-annotation, .delete-annotation *')) {
 						removeAnnotation(annotation.id);
 					}
-					if (e.target.matches('.save-annotation, .save-annotation *')) {
-						saveAnnotation(annotation);
-					}
-
 				});
 
 				//store in collection
@@ -142,32 +124,16 @@ function updateScreenPosition(annotation) {
 	annotation.html.style.opacity = Math.min(Math.max(diff, 0.15), 1);
 }
 
-function saveAnnotation(annotation) {
-	state.annotations[annotation.id] = annotation;
-	window.localStorage.annotations = JSON.stringify(state.annotations);
-	console.log(window.localStorage);
-}
-
 function removeAnnotation(id) {
 	const elem = document.getElementById(id);
 
 	elem.parentNode.removeChild(elem);
 
 	delete state.annotations[id];
-	delete window.localStorage.annotations[id];
-
-	console.log(window.localStorage);
-}
-
-function clearStorage() {
-	window.localStorage.clear();
-	console.log(window.localStorage);
 }
 
 function bindEventListeners() {
 	window.onresize = resizeCanvas;
-	
-	document.getElementById('clear-storage').addEventListener('click', clearStorage);
 }
 
 function resizeCanvas() {
