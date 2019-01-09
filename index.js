d821989d701139e149ba081c0088ff9ed9678ef8
Konstantin Dolbilov <dolbilov@galaxy-labs.ru>
2019-01-09 20:12:18 +0400

    fix production

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const serveStatic = require('serve-static');
 const mongoose = require('mongoose');
 const logger = require('morgan');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 9999;
@@ -10,8 +11,15 @@ const port = process.env.PORT || 9999;
 
 //Middleware
 app.use(serveStatic(path.join(__dirname + '/dist')));
+app.use(cors());
 app.use(logger('dev'));
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('/dist'));
+	app.get('*', (request, response) => {
+		response.sendFile(path.join(__dirname, '/dist', 'index.html'));
+	});
+}
 
 //models
 require('./server/models');
