4fcca0a152847ed668424143be421add72e416eb
Konstantin Dolbilov <dolbilov@galaxy-labs.ru>
2019-02-11 19:11:11 +0400

    fix mesh bg

--- src/views/Scene.vue
+++ src/views/Scene.vue
@@ -206,7 +206,7 @@
 		left: 30%;
 		right: 30%;
 		bottom: 0;
-        background-color: rgba(1, 1, 1, 0.7);
+        background-color: rgba(255, 255, 255, 0.7);
         &.light {
 			background-color: rgba(0, 0, 0, 0.7);
 		}
