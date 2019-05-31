ec2745f7d07cefb9877e56b138f0507fdee9ca24
Reuben Schmidt <reuben.schmidt@icloud.com>
2019-05-31 17:26:12 +1000

    fix

--- src/components/navigation/SidebarModels.vue
+++ src/components/navigation/SidebarModels.vue
@@ -3,7 +3,6 @@
 		<a class="dropdown-btn" @click="dropdown.model = !dropdown.model">
 			Other Models
       <i v-if="!dropdown.model" class="sidenav-icon fa fa-caret-down"></i>
-      <i v-if="dropdown.model" class="sidenav-icon fa fa-caret-up"></i>
 		</a>
 		<div class="dropdown-container" v-show="dropdown.model">
 			<template v-for="model in modelList" >
@@ -43,15 +42,16 @@
 </script>
 
 <style lang="scss">
-  .sidenav .header {
-    padding: 20px 0px 0px 16px;
-    text-decoration: none;
-    display: block;
-  }
+	.sidenav .header {
+		padding: 20px 0px 0px 16px;
+		text-decoration: none;
+		display: block;
+	}
 
-  .dropdown-container {
-     padding-left: 16px;
-     overflow: auto;
-   }
+	.dropdown-container {
+    padding-left: 16px;
+		max-height: 40vh;
+		overflow: auto;
+	}
 
 </style>
